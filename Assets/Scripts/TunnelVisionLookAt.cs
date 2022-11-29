c811648d44b72eba8885998006d3a82cf5df8a8b
Olger-Klok <Olger.Klok@hva.nl>
2022-11-29 14:13:30 +0100

    Updated look at script

--- Assets/Scripts/TunnelVisionLookAt.cs
+++ Assets/Scripts/TunnelVisionLookAt.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class TunnelVisionLookAt : MonoBehaviour
 {
     public RectTransform target;
+    private Vector3 targetPosLastFrame = new Vector3(0,0,0);
     // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        transform.LookAt(target);
+        if (Mathf.Abs(target.transform.position.x - targetPosLastFrame.x) > 30)
+        {
+            transform.LookAt(target.position);
+            targetPosLastFrame = target.transform.position;
+        }
+        if (Mathf.Abs(target.transform.position.y - targetPosLastFrame.y) > 30)
+        {
+            transform.LookAt(target.position);
+            targetPosLastFrame = target.transform.position;
+        }
     }
 }
