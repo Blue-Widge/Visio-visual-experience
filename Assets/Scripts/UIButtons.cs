4cc14cc51ac2b7816b4bacbf770cc3e9ccd4db8d
Romy Knuppe <romy.knuppe@hva.nl>
2022-12-15 14:30:53 +0100

    Added Screencanvas + Script
    Added DontDestroy script
    Added UIButtons Script
    Added Screencanvas as Prefab

--- Assets/Scripts/UIButtons.cs
+++ Assets/Scripts/UIButtons.cs
@@ -5,19 +5,42 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class UIButtons : MonoBehaviour
 {
-    public GameObject eyeTracking;
-    public GameObject tunnelVision;
-    public TunnelingVignetteController tunnelingVignetteController;
+    [SerializeField]
+    private GameObject eyeTracking;
+    [SerializeField]
+    private GameObject tunnelVision;
+    [SerializeField]
+    private TunnelingVignetteController tunnelingVignetteController;
     public Slider tunnelVisionSlider;
     public TextMeshProUGUI tunnelVisionSizeText;
     private string tunnelVisionTextValue;
+    private static GameObject instance;
+    public Toggle eyeTrackingToggle;
+    public Toggle tunnelVisionToggle;
+    public bool updatedThisScene = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        DontDestroyOnLoad(this);
+        if (instance == null)
+        {
+            instance = this.gameObject;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        SceneManager.activeSceneChanged += SceneChanged;
+        eyeTracking = GameObject.Find("Tobii Gaze Visualizer").gameObject;
+        tunnelVision = GameObject.Find("TunnelingVignette").gameObject;
+        tunnelingVignetteController = tunnelVision.GetComponent<TunnelingVignetteController>();
         tunnelVisionSizeText.text = tunnelingVignetteController.defaultParameters.apertureSize.ToString();
         tunnelVisionSlider.value = tunnelingVignetteController.defaultParameters.apertureSize;
     }
@@ -25,7 +48,11 @@ public class UIButtons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (updatedThisScene)
+        {
+            TunnelVisionSize();
+            updatedThisScene = false;
+        }
     }
 
     public void ToggleEyeTracking()
@@ -41,7 +68,42 @@ public class UIButtons : MonoBehaviour
     public void TunnelVisionSize()
     {
         tunnelingVignetteController.defaultParameters.apertureSize = tunnelVisionSlider.value;
+        Debug.LogWarning("slider size= " + tunnelVisionSlider.value);
+        Debug.LogWarning("size= " + tunnelingVignetteController.defaultParameters.apertureSize);
         tunnelVisionTextValue = Math.Round(tunnelVisionSlider.value, 3).ToString();
         tunnelVisionSizeText.text = tunnelVisionTextValue;
+    }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void SwitchKitchen(int number) 
+    {
+        switch (number) 
+        {
+            case 0:
+                SceneManager.LoadScene("Kitchen");
+                break;
+            case 1:
+                SceneManager.LoadScene("KitchenMain");
+                break;
+            case 2:
+                SceneManager.LoadScene("SampleScene");
+                break;
+            default:
+                break;
+        }
+    }
+
+    void SceneChanged(Scene current, Scene next)
+    {
+        eyeTracking = GameObject.Find("Tobii Gaze Visualizer").gameObject;
+        tunnelVision = GameObject.Find("TunnelingVignette").gameObject;
+        tunnelingVignetteController = tunnelVision.GetComponent<TunnelingVignetteController>();
+        eyeTracking.SetActive(eyeTrackingToggle.isOn);
+        tunnelVision.SetActive(tunnelVisionToggle.isOn);
+        updatedThisScene = true;
     }
 }
