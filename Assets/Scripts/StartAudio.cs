4d2780bd55dfd9dbe64859c864dac6bd8c842208
Evelien <evelien.ates@hva.nl>
2023-01-10 13:25:49 +0100

    audio

--- Assets/Scripts/StartAudio.cs
+++ Assets/Scripts/StartAudio.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,23 +7,34 @@ public class StartAudio : MonoBehaviour
 {
     public AudioSource audioSource;
     private string hand = "Hand";
+    public AudioSource mandatoryPreviousSource;
 
+    private bool trigger;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (mandatoryPreviousSource == null)
+        {
+            trigger = true;
+        } else
+        {
+            trigger = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!trigger && mandatoryPreviousSource.isPlaying)
+        {
+            trigger = true;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         
-        if (other.CompareTag(hand))
+        if (other.CompareTag(hand) && trigger)
         {
             audioSource.Play();
         }
