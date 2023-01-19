1189a0fe0b28718ecac7d08f838ae442ded73e72
Mika Vels <mika.vels@hva.nl>
2023-01-19 11:04:54 +0100

    updatet cabinets to only play audio after controller explenation is finished, updated book model

--- Assets/Scripts/Audio/CabinetAudioController.cs
+++ Assets/Scripts/Audio/CabinetAudioController.cs
@@ -1,30 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class CabinetAudioController : MonoBehaviour
 {
     public AudioSource audioSource;
+    public AudioSource mandatoryPreviousSource;
     private HingeJoint hingeJoint;
+
     public int turnOnAngle;
     public bool playWhenSmaller;
     private bool played = false;
+    private float previousSourceLength = 0;
+
+    private bool trigger = false;
     // Start is called before the first frame update
     void Start()
     {
         hingeJoint = GetComponent<HingeJoint>();
+        previousSourceLength = mandatoryPreviousSource.clip.length;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!trigger && mandatoryPreviousSource.isPlaying) trigger = true;
+        if (trigger && previousSourceLength >= 0) previousSourceLength -= Time.deltaTime;
         if (audioSource.isPlaying) played = true;
-        if (playWhenSmaller && hingeJoint.angle <= turnOnAngle && !played)
+        if (trigger && playWhenSmaller && hingeJoint.angle <= turnOnAngle && !played)
         {
             played = true;
             AudioController.PlayAudio(audioSource, audioSource.clip, false);
         }
-        else if (!playWhenSmaller && hingeJoint.angle >= turnOnAngle && !played)
+        else if (trigger && !playWhenSmaller && hingeJoint.angle >= turnOnAngle && !played)
         {
             played = true;
             AudioController.PlayAudio(audioSource, audioSource.clip, false);
